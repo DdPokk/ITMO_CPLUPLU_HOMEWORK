4168ae0cd5e0ac188d15ac133fa64f5acee6e21a
DdPokk <9111322054@mail.ru>
2022-07-12 10:56:28 +0300

    обновлено

--- Lab_13.final.1/Lab_13.final.1.cpp
+++ Lab_13.final.1/Lab_13.final.1.cpp
@@ -1,85 +1,41 @@
 // Lab_13.final.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-#include <string>
-#include <windows.h> 
+#include <vector>
+#include <windows.h>
+#include "human.h"
+#include "student.h"
+#include "teacher.h"
 
-using namespace std;
-
-class Item 
+void ShowInfo(const human* hum) 
 {
-private:
-	string title;
-	double price;
-public:
-	virtual void getdata()
-	{
-		cout << "\nВведите заголовок : ";
-		cin >> title;
-		cout << "Введите цену : ";
-		cin >> price;
-	}
-	virtual void putdata()
-	{
-		cout << "\nЗаголовок: " << title;
-		cout << "\nЦена:" << price;
-	}
-};
-class Paperbook : public Item
-{
-private:
-	int pages;
-public:
-	void getdata()
-	{
-		Item::getdata();
-		cout << "Введите страницы : ";
-		cin >> pages;
-	}
-	void putdata()
-	{
-		Item::putdata();
-		cout << "\nСтраниц:" << pages;
-	}
-};
-class AudioBook : public Item
-{
-private:
-	double time;
-public:
-	void getdata()
-	{
-		Item::getdata();
-		cout << "Введите время звучания : ";
-		cin >> time;
-	}
-	void putdata()
-	{
-		Item::putdata();
-		cout << "\nВремя звучания : " << time;
-	}
-};
+	std::cout << hum->get_info() << "\n----------------- " << std::endl;
+}
 int main()
 {
 	SetConsoleOutputCP(1251);
-	Item* pubarr[100];
-	int n = 0;
-	char choice;
-	do
-	{
-		cout << "\nВводить данные для книги или звукового файла(b / a) ? ";
-		cin >> choice;
-		if (choice == 'b')
-			pubarr[n] = new Paperbook;
-		else
-			pubarr[n] = new AudioBook;
-		pubarr[n++]->getdata();
-		cout << "Продолжать((у / n) ? ";
-		cin >> choice;
-	} while (choice == 'y');
-
-	for (int j = 0; j < n; j++)
-		pubarr[j]->putdata();
+	SetConsoleCP(1251);
+	std::vector<int> scores;
+	scores.push_back(5);
+	scores.push_back(3);
+	scores.push_back(4);
+	scores.push_back(4);
+	scores.push_back(5);
+	scores.push_back(3);
+	scores.push_back(3);
+	scores.push_back(3);
+	scores.push_back(3);
+	human* hum1 = new human("Иванов", "Иван", "Иванович");
+	std::cout << hum1->get_info() << "\n----------------- " << std::endl;
+	student* stud = new student("Петров", "Петр", "Петрович", scores);
+	hum1 = stud;
+	std::cout << hum1->get_info() << "\n----------------- " << std::endl;
+	unsigned int teacher_work_time = 40;
+	teacher* tch = new teacher("Сергеев", "Сергей", "Сергеевич", teacher_work_time);
+	hum1 = tch;
+	std::cout << hum1->get_info() << "\n----------------- " << std::endl;
+	delete hum1;
+	delete stud;
+	delete tch;
 	return 0;
 }
--- Lab_14.final/Lab_14.final.cpp
+++ Lab_14.final/Lab_14.final.cpp
@@ -3,7 +3,36 @@
 
 #include <iostream>
 
+using namespace std;
+
+template<class Class>
+void sorting(Class arr[], int size) 
+{
+    int j = 0;
+    for (int i = 0; i < size; i++) 
+    {
+        int x = arr[i];
+        for (j = i - 1; j >= 0 && x < arr[j]; j--)
+            arr[j + 1] = arr[j];
+        arr[j + 1] = x;
+    }
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int arr[] = { 88,1,32,24,7,6 };
+    char arrchar[] = "ffffffffggggggggaaaaaaaa";
+    double arrdouble[] = { 1.1, 5.2,0.7,8.4,9.3,3.33 };
+    int k1 = sizeof(arr) / sizeof(arr[0]);
+    int k2 = sizeof(arrdouble) / sizeof(arrdouble[0]);
+    int k3 = sizeof(arrchar) / sizeof(arrchar[0]) - 1;
+    sorting(arr, k1);
+    for (int i = 0; i < k1; i++) cout << arr[i] << ";";
+    cout << endl;
+    sorting(arrdouble, k2);
+    for (int i = 0; i < k2; i++) cout << arrdouble[i] << ";";
+    cout << endl;
+    sorting(arrchar, k3);
+    for (int i = 0; i < k3; i++) cout << arrchar[i] << ";";
+    cout << endl;
 }
